a9a63deaa2da8c9f4141fdbb4bbb070e3ad82836
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2006-10-09 02:11:31 +0000

    TMidiKeys component added. Inspired by Tobybear.

---
+++ Source/DMidiKeys.pas
@@ -0,0 +1,275 @@
+unit DMidiKeys;
+{$R DMidiKeys.res}
+{$R-}
+
+interface
+
+uses Windows, Classes, Graphics, Forms, Controls, ExtCtrls, Messages;
+
+type
+  TMidiKeyEvent = procedure(Sender: TObject; Shift: TShiftState; X, Y, Key: Integer) of object;
+
+  TMidiKeys = class(TGraphicControl)
+  private
+    fBlackKeyHeight : Integer;
+    fBuffer         : TBitmap;
+    fKeysDown       : array[0..127] of Boolean;
+    fNumOctaves     : Word;
+    fBaseOct        : Integer;
+    fOnMidiKeyUp    : TMidiKeyEvent;
+    fOnMidiKeyDown  : TMidiKeyEvent;
+    fOnKeyDown      : TKeyEvent;
+    fOnKeyPress     : TKeyPressEvent;
+    fOnKeyUp        : TKeyEvent;
+    fLastNote       : Word;
+    procedure WMEraseBkgnd(var m: TWMEraseBkgnd); message WM_ERASEBKGND;
+    procedure SetNumOctaves(const Value: word);
+    procedure SetBaseOct(const Value: integer);
+    function GetKeysDown(index: Integer): Boolean;
+    procedure SetKeysDown(index: Integer; const Value: Boolean);
+  protected
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseMove(Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer); override;
+    procedure WMWindowPosChanged(var Message: TWMWindowPosChanged); message WM_WINDOWPOSCHANGED;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+    procedure Paint; override;
+    function PositionToNote(x,y: Integer): integer;
+    property KeyDown[index:Integer]: Boolean read GetKeysDown write SetKeysDown;
+  published
+    property BaseOctave: Integer read FBaseOct write SetBaseOct default 2;
+    property NumOctaves: Word read FNumOctaves write SetNumOctaves default 3;
+    property Anchors;
+    property Align;
+    property Constraints;
+    property Enabled;
+    property Visible;
+    property OnMouseDown;
+    property OnMouseMove;
+    property OnMouseUp;
+    property OnKeyDown: TKeyEvent read FOnKeyDown write FOnKeyDown;
+    property OnKeyPress: TKeyPressEvent read FOnKeyPress write FOnKeyPress;
+    property OnKeyUp: TKeyEvent read FOnKeyUp write FOnKeyUp;
+    property OnMidiKeyDown: TMidiKeyEvent read FOnMidiKeyDown write FOnMidiKeyDown;
+    property OnMidiKeyUp: TMidiKeyEvent read FOnMidiKeyUp write FOnMidiKeyUp;
+  end;
+
+procedure Register;
+
+implementation
+
+uses SysUtils;
+
+constructor TMidiKeys.Create(AOwner: TComponent);
+var i: integer;
+begin
+ inherited Create(AOwner);
+ fBaseOct := 2;
+ fBuffer := TBitmap.Create;
+ fNumOctaves := 3;
+ ControlStyle := ControlStyle+[csOpaque];
+ for i := 0 to 127 do fKeysDown[i] := false;
+end;
+
+destructor TMidiKeys.Destroy;
+begin
+ fBuffer.Destroy;
+ inherited;
+end;
+
+procedure TMidiKeys.Paint;
+var i,o : Integer;
+    s   : Single;
+    kd  : Boolean;
+begin
+ with fBuffer.Canvas do
+  begin
+   Pen.Color:=clBlack;
+   Pen.Style:=psSolid;
+
+   // Render white keys
+   s:=Width/(7*NumOctaves+1);
+   for i:=0 to 7*NumOctaves do
+    begin
+     o:=12*(i div 7); kd:=False;
+     case (i mod 7) of
+      0: kd:=fKeysDown[BaseOctave*12+o   ];
+      1: kd:=fKeysDown[BaseOctave*12+o+ 2];
+      2: kd:=fKeysDown[BaseOctave*12+o+ 4];
+      3: kd:=fKeysDown[BaseOctave*12+o+ 5];
+      4: kd:=fKeysDown[BaseOctave*12+o+ 7];
+      5: kd:=fKeysDown[BaseOctave*12+o+ 9];
+      6: kd:=fKeysDown[BaseOctave*12+o+11];
+     end;
+
+     if kd
+      then Brush.Color:=$D0D0D0
+      else Brush.Color:=$FFFFFF;
+     Rectangle(Round(i*s), 0, Round(i*s+s), Height);
+     Pen.Color:=clSilver; MoveTo(Round(i*s+s)-1,1);
+     LineTo(Round(i*s+s)-1,Height-1); Pen.Color:=clBlack;
+    end;
+
+   // Render black keys
+   Brush.Color:=$000000;
+   s:=((7*NumOctaves)/(7*NumOctaves+1))*Width/(12*NumOctaves);
+   for i:=0 to 12*NumOctaves+1 do
+    begin
+     if (i mod 12) in [1,3,6,8,10] then
+      begin
+       if fKeysDown[BaseOctave*12+i]
+        then Brush.Color:=$333333
+        else Brush.Color:=clBlack;
+       Rectangle(Round((i  )*s), 0, Round((i+1)*s+1), fBlackKeyHeight);
+       Pen.Color:=clGray;
+       MoveTo(Round(i*s),1);
+       LineTo(Round(i*s),fBlackKeyHeight-1);
+       LineTo(Round(i*s+s)+1,fBlackKeyHeight-1);
+       Pen.Color:=clBlack;
+      end;
+    end;
+  end;
+ with Canvas do
+  begin
+   CopyMode := cmSrcCopy;
+   Draw(0, 0, fBuffer);
+  end;
+end;
+
+procedure TMidiKeys.MouseDown(Button: TMouseButton; Shift: TShiftState;
+  X, Y: Integer);
+var i : Integer;
+begin
+ MouseCapture := True;
+ inherited MouseDown(Button, Shift, X, Y);
+ if (x < 0) or (x > width) or (y < 0) or (y > height) then exit;
+ i := PositionToNote(x, y);
+ if (i<0) or (i>127) then exit;
+
+ if (fKeysDown[i] = false) then
+ begin
+  if assigned(FOnMidiKeyDown) then FOnMidiKeyDown(self, shift, X, Y, i);
+  fKeysDown[i] := true;
+ end else
+ begin
+  if (ssCtrl in shift) then
+  begin
+   if assigned(FOnMidiKeyUp) then FOnMidiKeyUp(self, shift, X, Y, i);
+   fKeysDown[i] := false;
+  end;
+ end;
+ fLastNote := i;
+ Invalidate;
+end;
+
+procedure TMidiKeys.MouseMove(Shift: TShiftState; X, Y: Integer);
+var i : Integer;
+begin
+ inherited MouseMove(Shift, X, Y);
+ if (x < 0) or (x > Width) or (y < 0) or (y > Height) then Exit;
+ if ((ssLeft in Shift) or (ssMiddle in Shift)) and not (ssCtrl in Shift) then
+  begin
+   i := PositionToNote(x, y);
+   if (i<0) or (i>127) then Exit;
+
+   if (i <> fLastNote) then
+    begin
+     fKeysDown[fLastNote] := false;
+     if assigned(FOnMidiKeyDown)
+      then FOnMidiKeyUp(Self, Shift, X, Y, fLastNote);
+     fLastNote := i;
+     fKeysDown[i] := true;
+     if assigned(FOnMidiKeyUp)
+      then FOnMidiKeyDown(Self, Shift, X, Y, fLastNote);
+     Invalidate;
+    end;
+  end;
+end;
+
+procedure TMidiKeys.MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+begin
+ MouseCapture := False;
+ inherited MouseUp(Button, Shift, X, Y);
+ if (fKeysDown[fLastNote])and not (ssCtrl in Shift) then
+  begin
+   if assigned(FOnMidiKeyUp)
+    then FOnMidiKeyUp(self, shift, X, Y, fLastNote);
+   fKeysDown[fLastNote] := False;
+  end;
+ Invalidate;
+end;
+
+function TMidiKeys.PositionToNote(x, y: Integer): Integer;
+var virtWidth : Single;
+    i,o       : Integer;
+begin
+ if y<fBlackKeyHeight
+  then
+   begin
+    virtWidth:=Width*((7*NumOctaves)/(7*NumOctaves+1));
+    virtWidth:=virtWidth/(12*NumOctaves);
+    Result:=BaseOctave*12+round(x/virtWidth-0.5);
+   end
+  else
+   begin
+    i:=Round(x*(7*NumOctaves+1)/width-0.5);
+    o:=12*(i div 7);
+    case (i mod 7) of
+     0: Result:=BaseOctave*12+o   ;
+     1: Result:=BaseOctave*12+o+ 2;
+     2: Result:=BaseOctave*12+o+ 4;
+     3: Result:=BaseOctave*12+o+ 5;
+     4: Result:=BaseOctave*12+o+ 7;
+     5: Result:=BaseOctave*12+o+ 9;
+     6: Result:=BaseOctave*12+o+11;
+    end;
+   end;
+end;
+
+procedure TMidiKeys.SetBaseOct(const Value: integer);
+begin
+ if (Value < 0) or (Value > 10) then exit;
+ fBaseOct := Value;
+ while fBaseOct + fNumOctaves > 10 do SetNumOctaves(fNumOctaves - 1);
+end;
+
+procedure TMidiKeys.SetKeysDown(index: Integer; const Value: Boolean);
+begin
+ fKeysDown[index]:=Value;
+ Invalidate;
+end;
+
+function TMidiKeys.GetKeysDown(index: Integer): Boolean;
+begin
+ result:=fKeysDown[index];
+end;
+
+procedure TMidiKeys.SetNumOctaves(const Value: word);
+begin
+ if (Value < 1) or (Value > 10) then exit;
+ FNumOctaves := Value;
+ while fBaseOct + fNumOctaves > 10
+  do fNumOctaves := fNumOctaves - 1;
+ Invalidate;
+end;
+
+procedure TMidiKeys.WMEraseBkgnd(var m: TWMEraseBkgnd); begin m.Result := LRESULT(False); end;
+
+procedure TMidiKeys.WMWindowPosChanged(var Message: TWMWindowPosChanged);
+begin
+ if Assigned(fBuffer) then
+  begin
+   fBuffer.Width := Width;
+   fBuffer.Height := Height;
+   fBlackKeyHeight := Round(0.63*Height);
+  end;
+end;
+
+procedure Register;
+begin
+ RegisterComponents('Audio', [TMidiKeys]);
+end;
+
+end.
