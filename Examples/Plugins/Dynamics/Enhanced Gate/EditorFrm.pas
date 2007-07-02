eccfd1f5d1cda18feb7ae5b4ac4fdaf6224f155c
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2007-07-02 17:13:24 +0000

    Added Enhanced Gate

---
+++ Examples/Plugins/Dynamics/Enhanced Gate/EditorFrm.pas
@@ -0,0 +1,30 @@
+unit EditorFrm;
+
+interface
+
+uses 
+  Windows, Messages, SysUtils, Classes, Forms, DDSPBase, DVSTModule, Controls,
+  StdCtrls;
+
+type
+  TEditorForm = class(TForm)
+    LbThreshold: TLabel;
+    ScrollBar: TScrollBar;
+    LbdB: TLabel;
+    procedure ScrollBarChange(Sender: TObject);
+  public
+  end;
+
+implementation
+
+{$R *.DFM}
+
+uses SimpleGateDM;
+
+procedure TEditorForm.ScrollBarChange(Sender: TObject);
+begin
+ TSimpleGateDataModule(Owner).Parameter[0] := ScrollBar.Position;
+ LbdB.Caption := IntToStr(ScrollBar.Position) + ' dB';
+end;
+
+end.
