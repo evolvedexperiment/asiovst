6db40c6c65ba67ec8f509b59e8f10ba7d1445852
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2007-09-23 12:19:56 +0000

    started to build a GUI for the multiband compressor

--- Examples/Plugins/Dynamics/Multiband Compressor/MBCGUI.dfm
+++ Examples/Plugins/Dynamics/Multiband Compressor/MBCGUI.dfm
@@ -1,10 +1,10 @@
 object FmMBC: TFmMBC
-  Left = 446
-  Top = 164
+  Left = 248
+  Top = 126
   BorderStyle = bsNone
   Caption = 'FmMBC'
-  ClientHeight = 110
-  ClientWidth = 252
+  ClientHeight = 307
+  ClientWidth = 775
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,6 +13,44 @@ object FmMBC: TFmMBC
   Font.Style = []
   OldCreateOrder = False
   Scaled = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  OnPaint = FormPaint
+  OnResize = FormResize
+  DesignSize = (
+    775
+    307)
   PixelsPerInch = 96
   TextHeight = 13
+  object LbAbout1: TLabel
+    Left = 48
+    Top = 10
+    Width = 168
+    Height = 15
+    Caption = 'D3 Multi Band Compressor'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -12
+    Font.Name = 'Courier New'
+    Font.Style = []
+    ParentFont = False
+    Transparent = True
+  end
+  object LbAbout2: TLabel
+    Left = 591
+    Top = 11
+    Width = 168
+    Height = 14
+    Alignment = taRightJustify
+    Anchors = [akTop, akRight]
+    Caption = 'delphi asio vst presents'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Courier New'
+    Font.Style = []
+    ParentFont = False
+    Transparent = True
+    ExplicitLeft = 474
+  end
 end
--- Examples/Plugins/Dynamics/Multiband Compressor/MBCGUI.pas
+++ Examples/Plugins/Dynamics/Multiband Compressor/MBCGUI.pas
@@ -3,10 +3,19 @@ unit MBCGUI;
 interface
 
 uses 
-  Windows, Messages, SysUtils, Classes, Forms, DDSPBase, DVSTModule;
+  Windows, Messages, SysUtils, Classes, Forms, DDSPBase, Graphics, DVSTModule,
+  Controls, StdCtrls;
 
 type
   TFmMBC = class(TForm)
+    LbAbout1: TLabel;
+    LbAbout2: TLabel;
+    procedure FormResize(Sender: TObject);
+    procedure FormPaint(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    fBackground : TBitmap;
   public
     MBCDataModule: TVSTModule;
   end;
@@ -14,5 +23,49 @@ type
 implementation
 
 {$R *.DFM}
+
+procedure TFmMBC.FormCreate(Sender: TObject);
+begin
+ fBackground := TBitmap.Create;
+end;
+
+procedure TFmMBC.FormDestroy(Sender: TObject);
+begin
+ fBackground.Free;
+end;
+
+procedure TFmMBC.FormPaint(Sender: TObject);
+begin
+ Canvas.Draw(0, 0, fBackground);
+end;
+
+procedure TFmMBC.FormResize(Sender: TObject);
+var x, y : Integer;
+begin
+ with fBackground do
+  begin
+   Width := ClientWidth;
+   Height := ClientHeight;
+   for x := 0 to Width - 1 do
+    for y := 0 to Height - 1 do
+     if (x mod 2 = 0) and (y mod 2 = 0)
+      then Canvas.Pixels[X, Y] := $A0A0A0
+      else Canvas.Pixels[X, Y] := $C0C0C0;
+   Canvas.MoveTo(10, 30);
+   Canvas.LineTo(Width - 10, 30);
+   Canvas.Brush.Color := $D0D0D0;
+   Canvas.Pen.Color   := $909090;
+   Canvas.Rectangle(20, 40, 251, 181);
+   Canvas.Rectangle(260, 40, 326, 181);
+   Canvas.Rectangle(335, 40, 401, 181);
+   Canvas.Rectangle(410, 40, 521, 181);
+   Canvas.Rectangle(530, 40, 641, 181);
+   Canvas.Rectangle(650, 40, 761, 181);
+
+   Canvas.Rectangle(20, 190, 261, 300);
+   Canvas.Rectangle(270, 190, 511, 300);
+   Canvas.Rectangle(520, 190, 761, 300);
+  end;
+end;
 
 end.
