57015c3bd691833735ee124c8786afa237a7ed12
the-real-myco <the-real-myco@885d5200-cde2-4488-8a86-08278623c11c>
2007-11-12 16:13:13 +0000

    removed old absolute overlay in buffer math (pascal)

--- Source/DAVDBufferMathPascal.pas
+++ Source/DAVDBufferMathPascal.pas
@@ -162,168 +162,124 @@ implementation
 EVERYTHING FOR SINGLE
 --------------------------------------------------------------------------------------------}
 procedure AddArrays(const input1, input2, output: TAVDSingleDynArray;const dim2: integer);
-var inp1: TAVDSingleDynArray absolute input1;
-    inp2: TAVDSingleDynArray absolute input2;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] + inp2[j];
+    output[j] := input1[j] + input2[j];
 end;
 
 procedure SubArrays(const from, amount, output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute from;
-    inp2: TAVDSingleDynArray absolute amount;
+var input1: TAVDSingleDynArray absolute from;
+    input2: TAVDSingleDynArray absolute amount;
     outp: TAVDSingleDynArray absolute output;
     j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] - inp2[j];
+    output[j] := input1[j] - input2[j];
 end;
 
 procedure MulArrays(const input1, input2,output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute input1;
-    inp2: TAVDSingleDynArray absolute input2;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] * inp2[j];
+    output[j] := input1[j] * input2[j];
 end;
 
 procedure AddArrays(const input1: TAVDSingleDynArray; const input2: single; const output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute input1;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] + input2;
+    output[j] := input1[j] + input2;
 end;
 
 procedure SubArrays(const from:   TAVDSingleDynArray; const amount: single; const output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute from;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] - amount;
+    output[j] := from[j] - amount;
 end;
 
 procedure MulArrays(const input1: TAVDSingleDynArray; const input2: single; const output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute input1;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] * input2;
+    output[j] := input1[j] * input2;
 end;
 
 
 procedure MulAddArrays(const factor1, factor2, summand, output: TAVDSingleDynArray; const dim2: integer);
-var fac1: TAVDSingleDynArray absolute factor1;
-    fac2: TAVDSingleDynArray absolute factor2;
-    summ: TAVDSingleDynArray absolute summand;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * fac2[j] + summ[j];
+    output[j] := factor1[j] * factor2[j] + summand[j];
 end;
 
 procedure MulAddArrays(const factor1: TAVDSingleDynArray; const factor2: single; const summand, output: TAVDSingleDynArray; const dim2: integer);
-var fac1: TAVDSingleDynArray absolute factor1;
-    summ: TAVDSingleDynArray absolute summand;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * factor2 + summ[j];
+    output[j] := factor1[j] * factor2 + summand[j];
 end;
 
 procedure MulAddArrays(const factor1, factor2: TAVDSingleDynArray; const summand: single; const output: TAVDSingleDynArray; const dim2: integer);
-var fac1: TAVDSingleDynArray absolute factor1;
-    fac2: TAVDSingleDynArray absolute factor2;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * fac2[j] + summand;
+    output[j] := factor1[j] * factor2[j] + summand;
 end;
 
 procedure MulAddArrays(const factor1: TAVDSingleDynArray; const factor2, summand: single; const output: TAVDSingleDynArray; const dim2: integer);
-var fac1: TAVDSingleDynArray absolute factor1;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * factor2 + summand;
+    output[j] := factor1[j] * factor2 + summand;
 end;
 
 procedure AddMulArrays(const summand1, summand2, factor, output: TAVDSingleDynArray; const dim2: integer);
-var sum1: TAVDSingleDynArray absolute summand1;
-    sum2: TAVDSingleDynArray absolute summand2;
-    fact: TAVDSingleDynArray absolute factor;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + sum2[j]) * fact[j];
+    output[j] := (summand1[j] + summand2[j]) * factor[j];
 end;
 
 procedure AddMulArrays(const summand1: TAVDSingleDynArray; const summand2: single; const factor, output: TAVDSingleDynArray; const dim2: integer);
-var sum1: TAVDSingleDynArray absolute summand1;
-    fact: TAVDSingleDynArray absolute factor;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + summand2) * fact[j];
+    output[j] := (summand1[j] + summand2) * factor[j];
 end;
 
 procedure AddMulArrays(const summand1, summand2: TAVDSingleDynArray; const factor: single; const output: TAVDSingleDynArray; const dim2: integer);
-var sum1: TAVDSingleDynArray absolute summand1;
-    sum2: TAVDSingleDynArray absolute summand2;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + sum2[j]) * factor;
+    output[j] := (summand1[j] + summand2[j]) * factor;
 end;
 
 procedure AddMulArrays(const summand1: TAVDSingleDynArray; const summand2, factor: single; const output: TAVDSingleDynArray; const dim2: integer);
-var sum1: TAVDSingleDynArray absolute summand1;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + summand2) * factor;
+    output[j] := (summand1[j] + summand2) * factor;
 end;
 
 
 
 
 procedure AddScaledArrays(const input1, input2: TAVDSingleDynArray; const factor1, factor2: single; const output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute input1;
-    inp2: TAVDSingleDynArray absolute input2;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j]*factor1 + inp2[j]*factor2;
+    output[j] := input1[j]*factor1 + input2[j]*factor2;
 end;
 
 
 
 procedure AddModulatedArrays(const input1, input2, envelope1, envelope2: TAVDSingleDynArray; const output: TAVDSingleDynArray; const dim2: integer);
-var inp1: TAVDSingleDynArray absolute input1;
-    inp2: TAVDSingleDynArray absolute input2;
-    env1: TAVDSingleDynArray absolute envelope1;
-    env2: TAVDSingleDynArray absolute envelope2;
-    outp: TAVDSingleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j]*env1[j] + inp2[j]*env2[j];
+    output[j] := input1[j]*envelope1[j] + input2[j]*envelope2[j];
 end;
 
 
@@ -489,168 +445,121 @@ EVERYTHING AGAIN FOR DOUBLE
 --------------------------------------------------------------------------------------------}
 
 procedure AddArrays(const input1, input2, output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute input1;
-    inp2: TAVDDoubleDynArray absolute input2;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] + inp2[j];
+    output[j] := input1[j] + input2[j];
 end;
 
 procedure SubArrays(const from, amount, output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute from;
-    inp2: TAVDDoubleDynArray absolute amount;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] - inp2[j];
+    output[j] := from[j] - amount[j];
 end;
 
 procedure MulArrays(const input1, input2,output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute input1;
-    inp2: TAVDDoubleDynArray absolute input2;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] * inp2[j];
+    output[j] := input1[j] * input2[j];
 end;
 
 procedure AddArrays(const input1: TAVDDoubleDynArray; const input2: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute input1;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] + input2;
+    output[j] := input1[j] + input2;
 end;
 
 procedure SubArrays(const from:   TAVDDoubleDynArray; const amount: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute from;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] - amount;
+    output[j] := from[j] - amount;
 end;
 
 procedure MulArrays(const input1: TAVDDoubleDynArray; const input2: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute input1;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j] * input2;
+    output[j] := input1[j] * input2;
 end;
 
 
 procedure MulAddArrays(const factor1, factor2, summand, output: TAVDDoubleDynArray; const dim2: integer);
-var fac1: TAVDDoubleDynArray absolute factor1;
-    fac2: TAVDDoubleDynArray absolute factor2;
-    summ: TAVDDoubleDynArray absolute summand;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * fac2[j] + summ[j];
+    output[j] := factor1[j] * factor2[j] + summand[j];
 end;
 
 procedure MulAddArrays(const factor1: TAVDDoubleDynArray; const factor2: Double; const summand, output: TAVDDoubleDynArray; const dim2: integer);
-var fac1: TAVDDoubleDynArray absolute factor1;
-    summ: TAVDDoubleDynArray absolute summand;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * factor2 + summ[j];
+    output[j] := factor1[j] * factor2 + summand[j];
 end;
 
 procedure MulAddArrays(const factor1, factor2: TAVDDoubleDynArray; const summand: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var fac1: TAVDDoubleDynArray absolute factor1;
-    fac2: TAVDDoubleDynArray absolute factor2;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * fac2[j] + summand;
+    output[j] := factor1[j] * factor2[j] + summand;
 end;
 
 procedure MulAddArrays(const factor1: TAVDDoubleDynArray; const factor2, summand: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var fac1: TAVDDoubleDynArray absolute factor1;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := fac1[j] * factor2 + summand;
+    output[j] := factor1[j] * factor2 + summand;
 end;
 
 procedure AddMulArrays(const summand1, summand2, factor, output: TAVDDoubleDynArray; const dim2: integer);
-var sum1: TAVDDoubleDynArray absolute summand1;
-    sum2: TAVDDoubleDynArray absolute summand2;
-    fact: TAVDDoubleDynArray absolute factor;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + sum2[j]) * fact[j];
+    output[j] := (summand1[j] + summand2[j]) * factor[j];
 end;
 
 procedure AddMulArrays(const summand1: TAVDDoubleDynArray; const summand2: Double; const factor, output: TAVDDoubleDynArray; const dim2: integer);
-var sum1: TAVDDoubleDynArray absolute summand1;
-    fact: TAVDDoubleDynArray absolute factor;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + summand2) * fact[j];
+    output[j] := (summand1[j] + summand2) * fact[j];
 end;
 
 procedure AddMulArrays(const summand1, summand2: TAVDDoubleDynArray; const factor: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var sum1: TAVDDoubleDynArray absolute summand1;
-    sum2: TAVDDoubleDynArray absolute summand2;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + sum2[j]) * factor;
+    output[j] := (summand1[j] + summand2[j]) * factor;
 end;
 
 procedure AddMulArrays(const summand1: TAVDDoubleDynArray; const summand2, factor: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var sum1: TAVDDoubleDynArray absolute summand1;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := (sum1[j] + summand2) * factor;
+    output[j] := (summand1[j] + summand2) * factor;
 end;
 
 
 
 
 procedure AddScaledArrays(const input1, input2: TAVDDoubleDynArray; const factor1, factor2: Double; const output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute input1;
-    inp2: TAVDDoubleDynArray absolute input2;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j]*factor1 + inp2[j]*factor2;
+    output[j] := input1[j]*factor1 + input2[j]*factor2;
 end;
 
 
 
 procedure AddModulatedArrays(const input1, input2, envelope1, envelope2: TAVDDoubleDynArray; const output: TAVDDoubleDynArray; const dim2: integer);
-var inp1: TAVDDoubleDynArray absolute input1;
-    inp2: TAVDDoubleDynArray absolute input2;
-    env1: TAVDDoubleDynArray absolute envelope1;
-    env2: TAVDDoubleDynArray absolute envelope2;
-    outp: TAVDDoubleDynArray absolute output;
-    j: integer;
+var j: integer;
 begin
   for j:=0 to dim2-1 do
-    outp[j] := inp1[j]*env1[j] + inp2[j]*env2[j];
+    output[j] := input1[j]*envelope1[j] + input2[j]*envelope2[j];
 end;
 
 
@@ -825,6 +734,7 @@ procedure GetSums(const input: TAVDDoubleDynArray; out outputmin, outputmax: Dou
 begin
 
 end;
+
 procedure GetPeaks(const input: TAVDArrayOfSingleDynArray; out outputmin, outputmax: TAVDSingleDynArray; const dim1, dim2: integer);
 begin
 
