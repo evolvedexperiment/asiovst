049bec7f0cd07be1fd16a4e3177744f8d98fb7c0
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2008-08-27 19:45:07 +0000

    - added some missing files

---
+++ Source/DGuiLabel.pas
@@ -0,0 +1,224 @@
+unit DGuiLabel;
+
+interface
+
+{$I ASIOVST.INC}
+
+uses
+  Windows, Classes, Graphics, Forms, Messages, SysUtils, Controls, Consts,
+  DGuiBaseControl;
+
+type
+  TCustomGuiLabel = class(TBufferedGraphicControl)
+  private
+    fAntiAlias     : TGuiAntiAlias;
+    fAlignment     : TAlignment;
+    fCaption       : string;
+    fOSFactor      : Integer;
+    {$IFNDEF FPC}
+    fTransparent   : Boolean;
+    procedure SetTransparent(Value: Boolean); virtual;
+    {$ENDIF}
+    procedure SetAntiAlias(const Value: TGuiAntiAlias);
+    procedure SetCaption(const Value: string);
+    procedure SetAlignment(const Value: TAlignment);
+  protected
+    procedure RenderLabelToBitmap(Bitmap: TBitmap); virtual;
+    procedure RedrawBuffer(doBufferFlip: Boolean = False); override;
+  public
+    constructor Create(AOwner: TComponent); overload; override;
+    property AntiAlias: TGuiAntiAlias read fAntiAlias write SetAntiAlias default gaaNone;
+    property Alignment: TAlignment read fAlignment write SetAlignment default taLeftJustify;
+    property Caption: string read fCaption write SetCaption;
+    {$IFNDEF FPC}
+    property Transparent: Boolean read fTransparent write SetTransparent default False;
+    {$ENDIF}
+  end;
+
+  TGuiLabel = class(TCustomGuiLabel)
+  published
+    property Caption;
+    property Color;
+    property AntiAlias;
+    property Alignment;
+    property AutoSize;
+    property Enabled;
+    property Font;
+    property Align;
+    property Anchors;
+    property Constraints;
+    property ShowHint;
+    property Visible;
+    property PopupMenu;
+    property DragKind;
+    property DragCursor;
+    property DragMode;
+
+    property OnCanResize;
+    property OnClick;
+    property OnConstrainedResize;
+    property OnContextPopup;
+    property OnDblClick;
+    property OnDragDrop;
+    property OnDragOver;
+    property OnEndDock;
+    property OnEndDrag;
+    property OnMouseDown;
+    property OnMouseMove;
+    property OnMouseUp;
+    property OnMouseWheel;
+    property OnMouseWheelDown;
+    property OnMouseWheelUp;
+    property OnResize;
+    property OnStartDock;
+    property OnStartDrag;
+    property OnPaint;
+    property OnMouseEnter;
+    property OnMouseLeave;
+  end;
+
+implementation
+
+{ TCustomGuiLabel }
+
+constructor TCustomGuiLabel.Create(AOwner: TComponent);
+begin
+ inherited;
+ fAntiAlias   := gaaNone;
+ fOSFactor    := 1;
+ fTransparent := False;
+ fAlignment   := taLeftJustify;
+end;
+
+procedure TCustomGuiLabel.RedrawBuffer(doBufferFlip: Boolean);
+var
+  Bmp : TBitmap;
+begin
+ // clear buffer
+ with fBuffer.Canvas do
+  begin
+   Brush.Color := Self.Color;
+   Font.Assign(Self.Font);
+   Font.Size := fOSFactor * Self.Font.Size;
+  end;
+
+ case fAntiAlias of
+  gaaNone     :
+   begin
+    RenderLabelToBitmap(fBuffer);
+    {$IFNDEF FPC}if fTransparent then DrawParentImage(fBuffer.Canvas) else{$ENDIF}
+    fBuffer.Canvas.FillRect(fBuffer.Canvas.ClipRect);
+   end;
+  gaaLinear2x :
+   begin
+    Bmp := TBitmap.Create;
+    with Bmp do
+     try
+      PixelFormat := pf32bit;
+      Width       := fOSFactor * fBuffer.Width;
+      Height      := fOSFactor * fBuffer.Height;
+      Canvas.Font.Assign(fBuffer.Canvas.Font);
+      Canvas.Brush.Assign(fBuffer.Canvas.Brush);
+      Canvas.Pen.Assign(fBuffer.Canvas.Pen);
+      {$IFNDEF FPC}
+      if fTransparent then
+       begin
+        DrawParentImage(Bmp.Canvas);
+        Upsample2xBitmap(Bmp);
+       end else
+      {$ENDIF}
+      Bmp.Canvas.FillRect(Bmp.Canvas.ClipRect);
+      RenderLabelToBitmap(Bmp);
+      Downsample2xBitmap(Bmp);
+      fBuffer.Canvas.Draw(0, 0, Bmp);
+     finally
+      Free;
+     end;
+   end;
+  gaaLinear4x :
+   begin
+    Bmp := TBitmap.Create;
+    with Bmp do
+     try
+      PixelFormat := pf32bit;
+      Width       := fOSFactor * fBuffer.Width;
+      Height      := fOSFactor * fBuffer.Height;
+      Canvas.Font.Assign(fBuffer.Canvas.Font);
+      Canvas.Brush.Assign(fBuffer.Canvas.Brush);
+      Canvas.Pen.Assign(fBuffer.Canvas.Pen);
+      {$IFNDEF FPC}
+      if fTransparent then
+       begin
+        DrawParentImage(Bmp.Canvas);
+        Upsample2xBitmap(Bmp);
+        Upsample2xBitmap(Bmp);
+       end else
+      {$ENDIF}
+      Bmp.Canvas.FillRect(Bmp.Canvas.ClipRect);
+      RenderLabelToBitmap(Bmp);
+      Downsample2xBitmap(Bmp);
+      Downsample2xBitmap(Bmp);
+      fBuffer.Canvas.Draw(0, 0, Bmp);
+     finally
+      Free;
+     end;
+   end;
+ end;
+end;
+
+procedure TCustomGuiLabel.RenderLabelToBitmap(Bitmap: TBitmap);
+var
+  TextSize : TSize;
+begin
+ TextSize := Bitmap.Canvas.TextExtent(fCaption);
+ case fAlignment of
+   taLeftJustify : Bitmap.Canvas.TextOut(0, 0, fCaption);
+  taRightJustify : Bitmap.Canvas.TextOut(Bitmap.Width - TextSize.cx, 0, fCaption);
+        taCenter : Bitmap.Canvas.TextOut((Bitmap.Width - TextSize.cx) div 2, 0, fCaption);
+ end;
+end;
+
+procedure TCustomGuiLabel.SetAlignment(const Value: TAlignment);
+begin
+ if fAlignment <> Value then
+  begin
+   fAlignment := Value;
+   RedrawBuffer(True);
+  end;
+end;
+
+procedure TCustomGuiLabel.SetAntiAlias(const Value: TGuiAntiAlias);
+begin
+ if fAntiAlias <> Value then
+  begin
+   fAntiAlias := Value;
+   case fAntiAlias of
+        gaaNone : fOSFactor := 1;
+    gaaLinear2x : fOSFactor := 2;
+    gaaLinear4x : fOSFactor := 4;
+   end;
+   RedrawBuffer(True);
+  end;
+end;
+
+procedure TCustomGuiLabel.SetCaption(const Value: string);
+begin
+ if fCaption <> Value then
+  begin
+   fCaption := Value;
+   RedrawBuffer(True);
+  end;
+end;
+
+{$IFNDEF FPC}
+procedure TCustomGuiLabel.SetTransparent(Value: Boolean);
+begin
+ if fTransparent <> Value then
+  begin
+   fTransparent := Value;
+   RedrawBuffer(True);
+  end;
+end;
+{$ENDIF}
+
+end.
---
+++ Source/DGuiPanel.pas
@@ -0,0 +1,337 @@
+unit DGuiPanel;
+
+interface
+
+{$I ASIOVST.INC}
+
+uses
+  Windows, Classes, Controls, DGuiBaseControl, Graphics;
+
+type
+  TCustomGuiPanel = class(TCustomGuiBaseControl)
+  private
+    fRoundRadius    : Integer;
+    fAntiAlias      : TGuiAntiAlias;
+    fOSFactor       : Integer;
+    procedure SetRoundRadius(Value: Integer);
+    procedure RenderGroupToBitmap(Bitmap: TBitmap);
+    procedure SetAntiAlias(const Value: TGuiAntiAlias);
+  protected
+    procedure RedrawBuffer(doBufferFlip: Boolean = False); override;
+  public
+    constructor Create(AOwner: TComponent); override;
+    property AntiAlias: TGuiAntiAlias read fAntiAlias write SetAntiAlias default gaaNone;
+    property Radius: Integer read fRoundRadius write SetRoundRadius default 2;
+    property LineColor default clBtnShadow;
+  end;
+
+  TGuiPanel = class(TCustomGuiPanel)
+  published
+    property Align;
+    property Anchors;
+    property AntiAlias;
+    property Color;
+    property Constraints;
+    property DragCursor;
+    property DragKind;
+    property DragMode;
+    property Enabled;
+    property Font;
+    property HeaderMinWidth;
+    property LineColor;
+    property LineWidth;
+    property PopupMenu;
+    property Radius;
+    property ShowHint;
+    property Transparent;
+    property Visible;
+
+    property OnCanResize;
+    property OnClick;
+    property OnConstrainedResize;
+    property OnContextPopup;
+    property OnDblClick;
+    property OnDragDrop;
+    property OnDragOver;
+    property OnEndDock;
+    property OnEndDrag;
+    property OnResize;
+    property OnStartDock;
+    property OnStartDrag;
+    property OnPaint;
+  end;
+
+implementation
+
+uses
+  Math, DAVDCommon, DAVDComplex;
+
+{ TCustomGuiPanel }
+
+constructor TCustomGuiPanel.Create(AOwner: TComponent);
+begin
+ inherited;
+ ControlStyle    := ControlStyle + [csFramed, csOpaque, csReplicatable,
+                                    csAcceptsControls];
+ fRoundRadius    := 2;
+ fHeaderMinWidth := 32;
+ fCaption        := 'Group'; //Name;
+ fLineColor      := clBtnShadow;
+end;
+
+procedure TCustomGuiPanel.RenderGroupToBitmap(Bitmap: TBitmap);
+var
+  Val, Off : TComplexDouble;
+  Steps, i : Integer;
+  rct      : TRect;
+  tmp      : Single;
+  PtsArray : Array of TPoint;
+begin
+ with Bitmap.Canvas do
+  begin
+   Lock;
+   Brush.Style := bsClear;
+   Brush.Color := fLineColor;
+   Pen.Width   := fOSFactor * fLineWidth;
+   Pen.Color   := fLineColor;
+   Font.Assign(Self.Font);
+   Font.Size := fOSFactor * Font.Size;
+
+   case fRoundRadius of
+    0, 1 : begin
+            FrameRect(ClipRect);
+            FillRect(Rect(1, 1, TextWidth(fCaption) + 12, TextHeight(fCaption) + 4));
+            MoveTo(1, TextHeight(fCaption) + 4);
+            LineTo(TextWidth(fCaption) + 11, TextHeight(fCaption) + 4);
+           end;
+       2 : begin
+            with ClipRect do
+             PolyLine([Point(Left  + 1, Bottom - 2), Point(Left     , Bottom - 3),
+                       Point(Left     , Top    + 2), Point(Left  + 2, Top       ),
+                       Point(Right - 3, Top       ), Point(Right - 1, Top    + 2),
+                       Point(Right - 2, Top    + 1), Point(Right - 1, Top    + 2),
+                       Point(Right - 1, Bottom - 2), Point(Right - 3, Bottom - 1),
+                       Point(Left  + 2, Bottom - 1), Point(Left,      Bottom - 3)]);
+            FillRect(Rect(1, 1, TextWidth(fCaption) + 12, TextHeight(fCaption) + 4));
+            MoveTo(1, TextHeight(fCaption) + 4);
+            LineTo(TextWidth(fCaption) + 11, TextHeight(fCaption) + 4);
+            // MoveTo(TextWidth(fCaption) + 12, 1);
+            // LineTo(TextWidth(fCaption) + 12, TextHeight(fCaption) + 3);
+           end;
+    else
+     begin
+      Steps := Round(2 / arcsin(1 / fRoundRadius)) + 1;
+      if Steps > 1 then
+      begin
+        SetLength(PtsArray, Steps + 4);
+        Val.Im := 0; Val.Re := -1;
+        Val.Re := Val.Re * fRoundRadius; Val.Im := Val.Im * fRoundRadius;
+
+        GetSinCos(2 * Pi / (Steps - 1), Off.Im, Off.Re);
+        PtsArray[0] := Point(Round(Linewidth div 2), Round(Linewidth div 2 + fRoundRadius));
+
+        // upper left corner
+        for i := 1 to Steps div 4 - 1 do
+         begin
+          tmp := Val.Re * Off.Re - Val.Im * Off.Im;
+          Val.Im := Val.Im * Off.Re + Val.Re * Off.Im;
+          Val.Re := tmp;
+          PtsArray[i] := Point(Round(Linewidth div 2 + fRoundRadius + Val.Re), Round(Linewidth div 2 + fRoundRadius + Val.Im));
+         end;
+        PtsArray[Steps div 4] := Point(Linewidth div 2 + fRoundRadius, Linewidth div 2 + 0);
+
+        // upper right corner
+        for i := Steps div 4 to Steps div 2 - 1 do
+         begin
+          tmp := Val.Re * Off.Re - Val.Im * Off.Im;
+          Val.Im := Val.Im * Off.Re + Val.Re * Off.Im;
+          Val.Re := tmp;
+          PtsArray[i + 1] := Point(Round(ClipRect.Right - fRoundRadius - (Linewidth + 1) div 2 + Val.Re), Round(Linewidth div 2 + fRoundRadius + Val.Im));
+         end;
+        PtsArray[Steps div 2 + 1] := Point(ClipRect.Right - (Linewidth + 1) div 2, Linewidth div 2 + fRoundRadius);
+
+        // lower right corner
+        for i := Steps div 2 to 3 * Steps div 4 - 1 do
+         begin
+          tmp := Val.Re * Off.Re - Val.Im * Off.Im;
+          Val.Im := Val.Im * Off.Re + Val.Re * Off.Im;
+          Val.Re := tmp;
+          PtsArray[i + 2] := Point(Round(ClipRect.Right - fRoundRadius - (Linewidth + 1) div 2 + Val.Re), Round(ClipRect.Bottom - (Linewidth + 1) div 2 - fRoundRadius + Val.Im));
+         end;
+        PtsArray[3 * Steps div 4 + 2] := Point(ClipRect.Right - fRoundRadius - (Linewidth + 1) div 2, ClipRect.Bottom - (Linewidth + 1) div 2);
+
+        // lower left corner
+        for i := 3 * Steps div 4 to Steps - 1 do
+         begin
+          tmp := Val.Re * Off.Re - Val.Im * Off.Im;
+          Val.Im := Val.Im * Off.Re + Val.Re * Off.Im;
+          Val.Re := tmp;
+          PtsArray[i + 3] := Point(Round(Linewidth div 2 + fRoundRadius + Val.Re), Round(ClipRect.Bottom - (Linewidth + 1) div 2 - fRoundRadius + Val.Im));
+         end;
+        PtsArray[Steps + 3] := Point(Linewidth div 2, fRoundRadius + Linewidth div 2);
+
+        PolyLine(PtsArray);
+
+        // Draw inner text
+        //////////////////
+
+        SetLength(PtsArray, Steps div 2 + 5);
+        Val.Re := -fRoundRadius; Val.Im := 0;
+
+        GetSinCos(2 * Pi / (Steps - 1), Off.Im, Off.Re);
+        rct := Rect(Linewidth div 2, Linewidth div 2, max(TextWidth(fCaption) + 10, fOSFactor * fHeaderMinWidth) - (Linewidth + 1) div 2, TextHeight(fCaption) + 5 - (Linewidth + 1) div 2);
+        PtsArray[0] := Point(Round(rct.Left), Round(rct.Top + fRoundRadius));
+
+        // upper left corner
+        for i := 1 to Steps div 4 - 1 do
+         begin
+          tmp := Val.Re * Off.Re - Val.Im * Off.Im;
+          Val.Im := Val.Im * Off.Re + Val.Re * Off.Im;
+          Val.Re := tmp;
+          PtsArray[i] := Point(Round(rct.Left + fRoundRadius + Val.Re), Round(rct.Top + fRoundRadius + Val.Im));
+         end;
+        PtsArray[Steps div 4] := Point(rct.Left + fRoundRadius, rct.Top);
+        PtsArray[Steps div 4  + 1 ] := Point(rct.Right, rct.Top);
+        PtsArray[Steps div 4  + 2 ] := Point(rct.Right, rct.Bottom - fRoundRadius);
+
+        Val.Re := fRoundRadius; Val.Im := 0;
+
+        // lower right corner
+        for i := Steps div 4 to Steps div 2 - 1 do
+         begin
+          tmp := Val.Re * Off.Re - Val.Im * Off.Im;
+          Val.Im := Val.Im * Off.Re + Val.Re * Off.Im;
+          Val.Re := tmp;
+          PtsArray[i + 3] := Point(Round(rct.Right - fRoundRadius + Val.Re), Round(rct.Bottom - fRoundRadius + Val.Im));
+         end;
+        PtsArray[Steps div 2 + 3] := Point(rct.Right - fRoundRadius, rct.Bottom);
+        PtsArray[Steps div 2 + 4] := Point(rct.Left, rct.Bottom);
+
+        Polygon(PtsArray);
+      end;
+     end;
+   end;
+
+   TextOut(6, 2, fCaption);
+   Unlock;
+  end;
+end;
+
+procedure TCustomGuiPanel.RedrawBuffer(doBufferFlip: Boolean);
+var
+  Bmp : TBitmap;
+begin
+ if (Width > 0) and (Height > 0) then with fBuffer.Canvas do
+  begin
+   Lock;
+   Brush.Style := bsSolid;
+   Brush.Color := Self.Color;
+   case fAntiAlias of
+    gaaNone     :
+     begin
+      {$IFNDEF FPC}if fTransparent then DrawParentImage(fBuffer.Canvas) else {$ENDIF}
+      FillRect(ClipRect);
+      RenderGroupToBitmap(fBuffer);
+     end;
+    gaaLinear2x :
+     begin
+      Bmp := TBitmap.Create;
+      with Bmp do
+       try
+        PixelFormat := pf32bit;
+        Width  := 2 * fBuffer.Width;
+        Height := 2 * fBuffer.Height;
+        Canvas.Brush.Style := bsSolid;
+        Canvas.Brush.Color := Self.Color;
+        Canvas.FillRect(Canvas.ClipRect);
+        {$IFNDEF FPC}
+        if fTransparent then
+         begin
+          DrawParentImage(Bmp.Canvas);
+          Upsample2xBitmap(Bmp.Canvas);
+         end else
+        {$ENDIF}
+        FillRect(ClipRect);
+        RenderGroupToBitmap(Bmp);
+        Downsample2xBitmap(Bmp);
+        Draw(0, 0, Bmp);
+       finally
+        Free;
+       end;
+     end;
+    gaaLinear4x :
+     begin
+      Bmp := TBitmap.Create;
+      with Bmp do
+       try
+        PixelFormat := pf32bit;
+        Width  := 4 * fBuffer.Width;
+        Height := 4 * fBuffer.Height;
+        Canvas.Brush.Style := bsSolid;
+        Canvas.Brush.Color := Self.Color;
+        Canvas.FillRect(Canvas.ClipRect);
+        {$IFNDEF FPC}
+        if fTransparent then
+         begin
+          DrawParentImage(Bmp.Canvas);
+          Upsample2xBitmap(Bmp.Canvas);
+          Upsample2xBitmap(Bmp.Canvas);
+         end else
+        {$ENDIF}
+        RenderGroupToBitmap(Bmp);
+        Downsample2xBitmap(Bmp);
+        Downsample2xBitmap(Bmp);
+        Draw(0, 0, Bmp);
+       finally
+        Free;
+       end;
+     end;
+   end;
+  end;
+
+ if doBufferFlip then Invalidate;
+end;
+
+procedure TCustomGuiPanel.SetAntiAlias(const Value: TGuiAntiAlias);
+begin
+ if fAntiAlias <> Value then
+  begin
+   fAntiAlias := Value;
+   case fAntiAlias of
+        gaaNone : fOSFactor := 1;
+    gaaLinear2x : fOSFactor := 2;
+    gaaLinear4x : fOSFactor := 4;
+   end;
+   RedrawBuffer(True);
+  end;
+end;
+
+procedure TCustomGuiPanel.SetCaption(const Value: string);
+begin
+ if fCaption <> Value then
+  begin
+   fCaption := Value;
+   RedrawBuffer(True);
+  end;
+end;
+
+procedure TCustomGuiPanel.SetHeaderMinWidth(const Value: Integer);
+begin
+ if fHeaderMinWidth <> Value then
+  begin
+   fHeaderMinWidth := Value;
+   RedrawBuffer(True);
+  end;
+end;
+
+procedure TCustomGuiPanel.SetRoundRadius(Value: Integer);
+begin
+ if Value < 0 then Value := 0;
+ if fRoundRadius <> Value then
+  begin
+   fRoundRadius := Value;
+   RedrawBuffer(True);
+  end;
+end;
+
+end.
