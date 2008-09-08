f4ea4bf963c98090e67e9da5f8785e1409c209d4
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2008-09-08 19:43:37 +0000

    - renamed directory as well

---
+++ Examples/Plugins/Dynamics/LA4029/LA4029.dpr
@@ -0,0 +1,37 @@
+{$J-,H+,T-P+,X+,B-,V-,O+,A+,W-,U-,R-,I-,Q-,D-,L-,Y-,C-}
+library LA4029;
+
+{$R 'LA4029.res' 'LA4029.rc'}
+
+uses
+  FastMM4,
+  Forms,
+  DVSTEffect,
+  DVSTModule,
+  LA4029DM in 'LA4029DM.pas' {LA4029DataModule: TVSTModule},
+  EditorFrm in 'EditorFrm.pas' {EditorForm},
+  DDSPLevelingAmplifier in '..\..\..\..\Source\DDSPLevelingAmplifier.pas',
+  DGuiVUMeter in '..\..\..\..\Source\DGuiVUMeter.pas';
+
+function main(audioMaster: TAudioMasterCallbackFunc): PVSTEffect; cdecl; export;
+var
+  LA4029DataModule: TLA4029DataModule;
+begin
+ try
+  LA4029DataModule := TLA4029DataModule.Create(Application);
+  LA4029DataModule.AudioMaster := audioMaster;
+  with LA4029DataModule do
+   begin
+    Effect^.user := LA4029DataModule;
+    Result := Effect;
+   end;
+ except
+  Result := nil;
+ end;
+end;
+
+exports Main name 'main';
+exports Main name 'VSTPluginMain';
+
+begin
+end.
