18d53e51d435102623c3be78b9afde74df6dd882
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2009-01-10 17:33:01 +0000

    - completed Cuda interface

--- Source/DSP/DAV_DspFftReal2ComplexCuda.pas
+++ Source/DSP/DAV_DspFftReal2ComplexCuda.pas
@@ -5,15 +5,22 @@ interface
 {$I DAV_Compiler.inc}
 
 uses
-  Classes, DAV_Common, DAV_Complex, DAV_CuFFT,
+  Classes, DAV_Common, DAV_Complex, DAV_CudaRuntime, DAV_CudaFFT,
   DAV_DspFftReal2Complex;
 
 type
   TFftReal2ComplexCuda32 = class(TFftReal2Complex)
   private
+    FGPUTimeDomain : Pointer;
+    FGPUFreqDomain : Pointer;
     procedure Rescale(const Data: PDAVSingleFixedArray);
     procedure RescaleSqrt(const Data: PDAVSingleFixedArray);
+  protected
+    procedure FFTOrderChanged; override;
   public
+    constructor Create; override;
+    constructor Create(const Order: Byte); override;
+    destructor Destroy; override;
     procedure PerformFFTCCS(const FrequencyDomain: PDAVComplexSingleFixedArray; const TimeDomain: PDAVSingleFixedArray);
     procedure PerformiFFTCCS(const FrequencyDomain: PDAVComplexSingleFixedArray; const TimeDomain: PDAVSingleFixedArray);
     procedure PerformFFT(const FrequencyDomain, TimeDomain: Pointer); override;
@@ -31,38 +38,141 @@ procedure TFftReal2ComplexCuda32.PerformFFTCCS(
   const FrequencyDomain: PDAVComplexSingleFixedArray;
   const TimeDomain: PDAVSingleFixedArray);
 var
-  CuFftHandle : TCuFftHandle;
-  CuFftResult : TCuFftResult;
+  CudaFftHandle : TCudaFftHandle;
+  CudaFftResult : TCudaFftResult;
+  CudaError     : TCudaError;
 begin
- CuFftHandle := 0;
- CuFftResult := CuFftPlan1d(CuFftHandle, FFftSize, CCuFftR2C, 1);
- if CuFftResult <> cfrSuccess
-  then raise Exception.Create(CCuFftResultStrings[CuFftResult]);
- CuFftResult := CuFftExecR2C(CuFftHandle, TimeDomain, FrequencyDomain);
- if CuFftResult <> cfrSuccess
-  then raise Exception.Create(CCuFftResultStrings[CuFftResult]);
- CuFftResult := CuFftDestroy(CuFftHandle);
- if CuFftResult <> cfrSuccess
-  then raise Exception.Create(CCuFftResultStrings[CuFftResult]);
+ CudaFftHandle := 0;
+
+ // create FFT plan
+ CudaFftResult := CudaFftPlan1d(CudaFftHandle, FFftSize, CCudaFftR2C, 1);
+ if CudaFftResult <> cfrSuccess
+  then raise Exception.Create(CCudaFftResultStrings[CudaFftResult]);
+
+ // copy memory from host to device
+ CudaError := CudaMemcpy(FGPUTimeDomain, TimeDomain, FFftSize * SizeOf(Single),
+   cmkHostToDevice);
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // perform FFT
+ CudaFftResult := CudaFftExecR2C(CudaFftHandle, FGPUTimeDomain, FGPUFreqDomain);
+ if CudaFftResult <> cfrSuccess
+  then raise Exception.Create(CCudaFftResultStrings[CudaFftResult]);
+
+ // copy memory from device to host
+ CudaError := CudaMemcpy(FrequencyDomain, FGPUFreqDomain, FFftSize * SizeOf(Single),
+   cmkDeviceToHost);
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // free FFT plan
+ CudaFftResult := CudaFftDestroy(CudaFftHandle);
+ if CudaFftResult <> cfrSuccess
+  then raise Exception.Create(CCudaFftResultStrings[CudaFftResult]);
+
 end;
 
 procedure TFftReal2ComplexCuda32.PerformiFFTCCS(
   const FrequencyDomain: PDAVComplexSingleFixedArray;
   const TimeDomain: PDAVSingleFixedArray);
 var
-  CuFftHandle: TCuFftHandle;
-  CuFftResult : TCuFftResult;
+  CudaFftHandle : TCudaFftHandle;
+  CudaFftResult : TCudaFftResult;
+  CudaError     : TCudaError;
 begin
- CuFftHandle := 0;
- CuFftResult := CuFftPlan1d(CuFftHandle, FFftSize, CCuFftC2R, 1);
- if CuFftResult <> cfrSuccess
-  then raise Exception.Create(CCuFftResultStrings[CuFftResult]);
- CuFftResult := CuFftExecR2C(CuFftHandle, FrequencyDomain, TimeDomain);
- if CuFftResult <> cfrSuccess
-  then raise Exception.Create(CCuFftResultStrings[CuFftResult]);
- CuFftResult := CuFftDestroy(CuFftHandle);
- if CuFftResult <> cfrSuccess
-  then raise Exception.Create(CCuFftResultStrings[CuFftResult]);
+ CudaFftHandle := 0;
+
+ // create FFT plan
+ CudaFftResult := CudaFftPlan1d(CudaFftHandle, FFftSize, CCudaFftC2R, 1);
+ if CudaFftResult <> cfrSuccess
+  then raise Exception.Create(CCudaFftResultStrings[CudaFftResult]);
+
+ // copy memory from host to device
+ CudaError := CudaMemcpy(FGPUFreqDomain, FrequencyDomain, FFftSize * SizeOf(Single),
+   cmkHostToDevice);
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // perform IFFT
+ CudaFftResult := CudaFftExecR2C(CudaFftHandle, FGPUFreqDomain, FGPUTimeDomain);
+ if CudaFftResult <> cfrSuccess
+  then raise Exception.Create(CCudaFftResultStrings[CudaFftResult]);
+
+ // copy memory from device to host
+ CudaError := CudaMemcpy(TimeDomain, FGPUTimeDomain, FFftSize * SizeOf(Single),
+   cmkDeviceToHost);
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // free FFT plan
+ CudaFftResult := CudaFftDestroy(CudaFftHandle);
+ if CudaFftResult <> cfrSuccess
+  then raise Exception.Create(CCudaFftResultStrings[CudaFftResult]);
+end;
+
+constructor TFftReal2ComplexCuda32.Create;
+begin
+ inherited;
+ FGPUTimeDomain := nil;
+ FGPUFreqDomain := nil;
+ FFTOrderChanged;
+end;
+
+constructor TFftReal2ComplexCuda32.Create(const Order: Byte);
+begin
+ inherited Create(Order);
+ FGPUTimeDomain := nil;
+ FGPUFreqDomain := nil;
+ FFTOrderChanged;
+end;
+
+destructor TFftReal2ComplexCuda32.Destroy;
+begin
+ if assigned(FGPUTimeDomain) then CudaFree(FGPUTimeDomain);
+ if assigned(FGPUFreqDomain) then CudaFree(FGPUFreqDomain);
+ inherited;
+end;
+
+procedure TFftReal2ComplexCuda32.FFTOrderChanged;
+var
+  OldPtr    : Pointer;
+  CudaError : TCudaError;
+begin
+ inherited;
+ // Time Domain
+ ///////////////////
+ OldPtr := FGPUTimeDomain;
+
+ // Allocate new memory
+ CudaError := CudaMalloc(FGPUTimeDomain, FFftSize * SizeOf(Single));
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // Clear memory
+ CudaError := CudaMemset(FGPUTimeDomain, 0, FFftSize * SizeOf(Single));
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // Free old memory
+ if assigned(OldPtr) then CudaFree(OldPtr);
+
+
+ // Frequency Domain
+ OldPtr := FGPUFreqDomain;
+
+ // Allocate new memory
+ CudaError := CudaMalloc(FGPUFreqDomain, (FFftSize div 2 + 1) * SizeOf(TComplexSingle));
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // Clear memory
+ CudaError := CudaMemset(FGPUFreqDomain, 0, FFftSize * SizeOf(Single));
+ if CudaError <> ceSuccess
+  then raise Exception.Create(CudaErrorToString(CudaError));
+
+ // Free old memory
+ if assigned(OldPtr) then CudaFree(OldPtr);
 end;
 
 procedure TFftReal2ComplexCuda32.PerformFFT(const FrequencyDomain,
